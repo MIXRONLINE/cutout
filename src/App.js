59863bc123ab51bf42696952a78a96495de942f3
Blaze Cerovcec <cerovcecblaze@gmail.com>
2021-01-14 14:24:24 -0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { render } from "react-dom";
+import React, { useEffect, useState } from 'react';
 import { Rnd } from "react-rnd";
 import './App.css'
 
